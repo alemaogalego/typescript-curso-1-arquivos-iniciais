855ede5a293a3d08ba4ea77c17bd3b1c472d39b1
Lucas César Lorena <lucascesar.lorena@gmail.com>
2026-01-19 17:45:15 -0300

    Remova os arquivos temporários e atualização do modelo de negociação.
    Excluí os arquivos temporários e de bloqueio do diretório de informações e adicionei uma linha em branco no construtor do modelo Negociacao para melhorar a legibilidade.

--- app/models/negociacao.ts
+++ app/models/negociacao.ts
@@ -5,6 +5,7 @@ export class Negociacao {
     private _data;
     private _quantidade;
     private _valor;
+    
     constructor(data, quantidade, valor) {
         this._data = data;
         this._quantidade = quantidade;
