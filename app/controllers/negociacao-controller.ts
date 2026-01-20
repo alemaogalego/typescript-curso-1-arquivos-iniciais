01256817921c48df6bf4c475cde145bb8a650554
Lucas César Lorena <lucascesar.lorena@gmail.com>
2026-01-20 12:14:39 -0300

    Add NegociacaoController and update TypeScript docs
    Apresenta o NegociacaoController para lidar com entradas de formulário e registro em TypeScript. Também atualiza e adiciona documentação relacionada em TypeScript e arquivos temporários.

---
+++ app/controllers/negociacao-controller.ts
@@ -0,0 +1,17 @@
+export class NegociacaoController {
+    private inputData;
+    private inputQuantidade;
+    private inputValor;
+
+    constructor() {
+        this.inputData = document.querySelector('#data');
+        this.inputQuantidade = document.querySelector('#quantidade');
+        this.inputValor = document.querySelector('#valor');
+    }
+    adiciona() {
+        console.log(this.inputData.value);
+        console.log(this.inputQuantidade.value);
+        console.log(this.inputValor.value);
+    }
+
+}
